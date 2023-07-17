0276bd7789e53b029f23ab9ef3eae7677a413f71
luisrodriguezgalvez <luis.rodriguez@deltares.nl>
2023-07-17 16:55:47 +0200

    text and image
    landing text updated and landing image added at the bottom

--- src/layouts/home.vue
+++ src/layouts/home.vue
@@ -29,15 +29,28 @@
               xl="6"
             >
               <h1 class="text-h4 mb-6">Grondwater Monitoring IJmuiden</h1>
-              <p class="text-body-1">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vel risus risus. Maecenas ac tincidunt sapien, eget aliquet mauris. Sed eleifend, metus ut maximus laoreet, neque dui volutpat neque, eu suscipit lectus sapien non lacus. Suspendisse vel nisl posuere, porttitor tortor ut, consequat lacus. Vestibulum eleifend egestas mi, eu pharetra nulla fringilla vitae. Vivamus ultricies magna eu eros vehicula pellentesque. Vestibulum elementum orci tempus nunc consequat, ac luctus nisl tristique. Praesent blandit porttitor vulputate. Aliquam vulputate, lectus nec interdum posuere, nunc enim auctor diam, eget elementum augue ex non enim. Vivamus eget interdum est. Proin luctus quis eros et pretium. Morbi ac augue orci.</p>
+              <p class="text-body-1">Welkom bij de webviewer voor grondwatermonitoring rondom het IJmuiden sluizencomplex, uitgevoerd door Deltares in opdracht van Rijkswaterstaat. Deze webviewer biedt inzicht in de monitoringsresultaten van het project en helpt bij het volgen van de verzilting in de komende jaren.
+                <br>Bij de aanleg van de Zeesluis IJmuiden en SO-IJ wordt de bodem van het Noordzeekanaal verdiept, waarbij een weerstandbiedende kleilaag wordt vergraven. Dit kan mogelijk leiden tot contact tussen zout zeewater en het zoete of brakke grondwater in het onderliggende watervoerende pakket. Hoewel de verzilting waarschijnlijk beperkt zal blijven tot binnen de kanaaloevers, willen we door middel van monitoring eventuele verzilting vroegtijdig constateren en volgen.
+                <br>De webviewer toont de monitoringsresultaten, waarbij er onderscheid wordt gemaakt tussen het Freatische pakket, het 1e watervoerende pakket (gescheiden door een kleilaag op ca. 17 m-NAP) en het 2e watervoerende pakket onder de Eemklei op ca. 40 m-NAP. Het 1e watervoerende pakket bevat momenteel zoet tot brak water, en de mogelijke verzilting van deze laag staat centraal in dit onderzoek.
+                <br>De webviewer biedt informatie over grondwaterstanden, geologie en waterkwaliteit. U kunt de gemeten grondwaterstanden per pakket bekijken, evenals boorgatmetingen en de resultaten van bemonstering en veldmetingen op elke meetlocatie.
+              </p>
               <v-btn
+                class="custom-btn"
                 :to="{ name: 'map' }"
                 color="primary"
-                depressed
               >
                 Bekijk de kaart
                 <v-icon right>mdi-arrow-right</v-icon>
               </v-btn>
+              <div class="container">
+                <v-img
+                  :lazy-src="image"
+                  :src="image"
+                  max-width="700"
+                  alt=""
+                  class="centered-img"
+                />
+              </div>
             </v-col>
           </v-row>
         </v-container>
@@ -61,8 +74,14 @@
 
 <script>
   import { mapActions } from 'vuex';
+  import image from '@/assets/img/figure_landing_text.png';
 
   export default {
+    data() {
+      return {
+        image: image,
+      };
+    },
     computed: {
       currentYear() {
         return new Date().getFullYear();
@@ -86,3 +105,22 @@
     },
   };
 </script>
+
+<style>
+.container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100;
+}
+
+.centered-img {
+  max-width: 700px;
+  margin-top: 30px;
+}
+
+.custom-btn {
+  min-height: 40px;
+  margin-top: 10px;
+}
+</style>
