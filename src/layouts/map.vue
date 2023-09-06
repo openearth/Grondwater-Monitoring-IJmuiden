33613bf50e9ad39616058ab95d382a19fe555c4f
luisrodriguezgalvez <77997772+luisrodriguezgalvez@users.noreply.github.com>
2023-09-06 16:50:37 +0200

    Update map.vue
    tab with dropdown almost finished, it is duplicated

--- src/layouts/map.vue
+++ src/layouts/map.vue
@@ -14,6 +14,32 @@
           Grondwater Monitoring IJmuiden
         </router-link>
       </v-toolbar-title>
+
+      <v-spacer />
+
+      <v-tabs
+      right
+      style="width: auto"
+      class="mr-15">
+        <v-tab @click="toggleDropdown">
+          Home page
+        </v-tab>
+        <v-menu
+        v-model="menuOpen"
+        :close-on-content-click="false"
+        offset-y>
+          <template v-slot:activator="{ on }">
+            <v-tab v-on="on">
+              Bevindingen
+            </v-tab>
+          </template>
+          <v-list>
+            <v-list-item @click="menuItemClicked('Item 1')"> Kaarten en figuren</v-list-item>
+            <v-list-item @click="menuItemClicked('Item 2')">Rapportages</v-list-item>
+          </v-list>
+        </v-menu>
+      </v-tabs>
+
     </v-app-bar>
 
     <v-main class="app-main">
@@ -51,5 +77,25 @@
     computed: {
       ...mapGetters('app', [ 'toastMessage' ]),
     },
+    data() {
+      return {
+        menuOpen: false,
+      };
+    },
+    methods: {
+      toggleDropdown() {
+        this.menuOpen = !this.menuOpen;
+      },
+      menuItemClicked(item) {
+        console.log(`Clicked: ${ item }`);
+      },
+    },
   };
 </script>
+
+<style scoped>
+/* Custom CSS to style the tab as a clickable element */
+.v-tabs .v-tab {
+  cursor: pointer;
+}
+</style>
